c9c912f7658d56e2ff36247ac81ee32fb010d67d
Jenny Wu <jennywu22553@gmail.com>
2025-02-12 21:17:41 -0600

    Implemented the addy method that adds a new SLNode with the given song at the end of the SLList and updates the pointers accordingly

--- src/SLList.java
+++ src/SLList.java
@@ -4,4 +4,17 @@ public class SLList {
     public SLList() {
         head = null;
     }
+
+    public void addy(Song s) {
+        SLNode newNode = new SLNode(s);
+        if (head == null) {
+            head = newNode;
+        } else {
+            SLNode temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
 }
