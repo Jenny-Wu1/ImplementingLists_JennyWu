a75cb346e5e8bdd4328bfa12895befc8e1fc1c76
Jenny Wu <jennywu22553@gmail.com>
2025-02-12 20:48:19 -0600

    Created addy method that takes in a song object and adds it to the array, resizing when the size of the array has reached maxSize

--- src/AList.java
+++ src/AList.java
@@ -7,4 +7,18 @@ public class AList {
         songs = new Song[maxSize];
         size = 0;
     }
+
+    public void addy(Song s) {
+        if (size == maxSize) {
+            int newSize = maxSize * 2;
+            Song[] newSongs = new Song[newSize];
+            for (int i = 0; i < size; i++) {
+                newSongs[i] = songs[i];
+            }
+            songs = newSongs;
+            maxSize = maxSize * 2;
+        }
+        songs[size] = s;
+        size++;
+    }
 }
